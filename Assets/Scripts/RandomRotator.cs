e7844f69866db0c35a323254742a1a153bbe6805
Thomas Killeen <C16394453@mydit.ie>
2018-04-11 16:10:09 +0100

    I added in an astroid and made a script for the astroid to rotate

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+    // Use this for initialization
+    public float tumble;
+	void Start ()
+    {
+        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;	
+	}
+}
